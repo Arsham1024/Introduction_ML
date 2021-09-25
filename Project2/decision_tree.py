74270e93cee1d0eaed7c1b1953eef148b314464e
Arsham1024 <Arsham512@gmail.com>
2021-09-25 16:46:17 -0700

    Made X and Y, need to import test set next

--- Project2/decision_tree.py
+++ Project2/decision_tree.py
@@ -10,9 +10,11 @@
 
 #importing some Python libraries
 from sklearn import tree
+import matplotlib.pyplot as plt
 import csv
 
-dataSets = ['contact_lens_training_1.csv', 'contact_lens_training_2.csv', 'contact_lens_training_3.csv']
+# I changed the path because the files are organized into traningfiles directory
+dataSets = ['./trainingfiles/contact_lens_training_1.csv', './trainingfiles/contact_lens_training_2.csv', './trainingfiles/contact_lens_training_3.csv']
 
 for ds in dataSets:
 
@@ -29,24 +31,53 @@ for ds in dataSets:
 
     #transform the original training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
     #--> add your Python code here
-    # X =
+
+    for row in range(len(dbTraining)):
+        X_r = []
+        for col in range(len(dbTraining[row])-1):
+            if dbTraining[row][col] == "Young":
+                X_r.append(1)
+            elif dbTraining[row][col] == "Presbyopic":
+                X_r.append(2)
+            elif dbTraining[row][col] == "Prepresbyopic":
+                X_r.append(3)
+            #     New attribute
+            elif dbTraining[row][col] == "Myope":
+                X_r.append(1)
+            elif dbTraining[row][col] == "Hypermetrope":
+                X_r.append(2)
+            #     New attribute
+            elif dbTraining[row][col] == "Yes":
+                X_r.append(1)
+            elif dbTraining[row][col] == "No":
+                X_r.append(2)
+            #     New Attribute
+            elif dbTraining[row][col] == "reduced":
+                X_r.append(1)
+            elif dbTraining[row][col] == "Normal":
+                X_r.append(2)
+        X.append(X_r)
+    print("Here is X:" , X)
 
     #transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
     #--> add your Python code here
-    # Y =
+    Y = [1 if row[-1] == "Yes" else 2 for row in dbTraining]
+    print("Here is Y:" , Y)
 
     #loop your training and test tasks 10 times here
     for i in range (10):
 
        #fitting the decision tree to the data setting max_depth=3
-       clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=3)
-       clf = clf.fit(X, Y)
+        clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=3)
+        clf = clf.fit(X, Y)
+        tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes', 'No'], filled=True, rounded=True)
+        plt.show()
 
        #read the test data and add this data to dbTest
        #--> add your Python code here
        # dbTest =
 
-       for data in dbTest:
+       # for data in dbTest:
            #transform the features of the test instances to numbers following the same strategy done during training, and then use the decision tree to make the class prediction. For instance:
            #class_predicted = clf.predict([[3, 1, 2, 1]])[0]           -> [0] is used to get an integer as the predicted class label so that you can compare it with the true label
            #--> add your Python code here
@@ -60,7 +91,3 @@ for ds in dataSets:
     #print the lowest accuracy of this model during the 10 runs (training and test set) and save it.
     #your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
     #--> add your Python code here
-
-
-
-
