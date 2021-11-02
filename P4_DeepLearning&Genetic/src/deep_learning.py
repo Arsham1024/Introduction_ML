38037c5678c78717f8f64056e6d22936f49a4df6
Arsham Mehrani <spectre@Arshams-MacBook-Pro.local>
2021-11-01 19:28:32 -0700

    bug fixed

--- P4_DeepLearning&Genetic/src/deep_learning.py
+++ P4_DeepLearning&Genetic/src/deep_learning.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
+# AUTHOR: Name
 # FILENAME: title of the source file
 # SPECIFICATION: description of the program
 # FOR: CS 4210- Assignment #4
@@ -8,7 +8,6 @@
 
 #IMPORTANT NOTE: YOU CAN USE ANY PYTHON LIBRARY TO COMPLETE YOUR CODE.
 
-#importing the libraries
 import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
@@ -20,21 +19,21 @@ def build_model(n_hidden, n_neurons_hidden, n_neurons_output, learning_rate):
     #-->add your Pyhton code here
 
     #Creating the Neural Network using the Sequential API
-    #model = keras.models.Sequential()
-    #model.add(keras.layers.Flatten(input_shape=[28, 28]))                                #input layer
+    model = keras.models.Sequential()
+    model.add(keras.layers.Flatten(input_shape=[28, 28]))                                #input layer
 
     #iterate over the number of hidden layers to create the hidden layers:
-    #model.add(keras.layers.Dense(n_neurons_hidden, activation="relu"))                   #hidden layer with ReLU activation function
+    model.add(keras.layers.Dense(n_neurons_hidden, activation="relu"))                   #hidden layer with ReLU activation function
 
     #output layer
-    #model.add(keras.layers.Dense(n_neurons_output, activation="softmax"))                #output layer with one neural for each class and the softmax activation function since the classes are exclusive
+    model.add(keras.layers.Dense(n_neurons_output, activation="softmax"))                #output layer with one neural for each class and the softmax activation function since the classes are exclusive
 
     #defining the learning rate
-    #opt = keras.optimizers.SGD(learning_rate)
+    opt = keras.optimizers.SGD(learning_rate)
 
     #Compiling the Model specifying the loss function and the optimizer to use.
-    #model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
-    #return model
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    return model
 
 
 #To install Tensor Flow on your terminal
@@ -59,20 +58,29 @@ n_hidden = [2, 5, 10]
 n_neurons = [10, 50, 100]
 l_rate = [0.01, 0.05, 0.1]
 
+highestAccuracy = 0
+accuracy = 0
 for h in n_hidden:                          #looking or the best parameters w.r.t the number of hidden layers
     for n in n_neurons:                     #looking or the best parameters w.r.t the number of neurons
         for l in l_rate:                    #looking or the best parameters w.r.t the learning rate
 
             #build the model for each combination by calling the function:
-            model = build_model()
+            model = build_model(h, n, 10, l)
 
             #To train the model
             history = model.fit(X_train, y_train, epochs=5, validation_data=(X_valid, y_valid))  #epochs = number times that the learning algorithm will work through the entire training dataset.
 
             #Calculate the accuracy of this neural network and store its value if it is the highest so far. To make a prediction, do:
             class_predicted = np.argmax(model.predict(X_test), axis=-1)
-            #-->add your Pyhton code here
+            correct = 0
+            for i in range(len(class_predicted)):
+                if class_predicted[i] == y_test[i]:
+                    correct += 1
+            accuracy = correct / len(X_test)
 
+            if accuracy > highestAccuracy:
+                highestAccuracy = accuracy
+            #print(class_predicted)
             print("Highest accuracy so far: " + str(highestAccuracy))
             print("Parameters: " + "Number of Hidden Layers: " + str(h) + ",number of neurons: " + str(n) + ",learning rate: " + str(l))
             print()
@@ -95,6 +103,5 @@ pd.DataFrame(history.history).plot(figsize=(8, 5))
 plt.grid(True)
 plt.gca().set_ylim(0, 1) # set the vertical range to [0-1]
 plt.show()
-
 
 
